b049e75178847b9a98c7be50c0400b4636788a2d
LaurentFaucher <laurent.faucher@polymtl.ca>
2020-09-30 21:09:49 -0400

    L.F: 1 à 6

--- exercice.py
+++ exercice.py
@@ -4,35 +4,72 @@
 
 def order(values: list = None) -> bool:
     if values is None:
-        # TODO: Demander les valeurs ici
-        pass
+        values = [input("Entrez ...") for _ in range (10)]
 
-    return False
+    return values == sorted(values)
 
 
 def anagrams(words: list = None) -> bool:
-    if words is None:
-        # TODO: Demander les mots ici
-        pass
 
-    return False
+    if words is None:
+        chaine1 = input()
+        chaine2 = input()
+        liste1, liste2 = [], []
+
+    for i in chaine1:
+         liste1.append(i)
+
+    for i in chaine2:
+        liste2.append(i)
+
+    liste1.sort()
+    liste2.sort()
+
+    return liste1 == liste2
 
 
 def contains_doubles(items: list) -> bool:
-    return False
+    unique = set(items)
+
+    return len(items) == len(unique)
 
 
 def best_grades(student_grades: dict) -> dict:
     # TODO: Retourner un dictionnaire contenant le nom de l'étudiant ayant la meilleure moyenne ainsi que sa moyenne
-    return {}
+
+    list_student, list_grades = [], []
+    nom, note = None, None
+
+    for student in student_grades:
+        student_grades[student] = sum(student_grades[student]) / len(student_grades[student])
+        list_student.append(student)
+        list_grades.append(student_grades)
+
+    for student in len(list_student):
+        if list_grades[student] > list_grades[student - 1]:
+            nom = list_student[student]
+            note = list_grades[studemt]
+
+    return nom, note
 
 
+#sentence = input("Donnez une phrase: ")
+#histogram(sentence)
 def histogram(sentence: str) -> tuple:
     # TODO: Créer l'histogramme a l'aide d'un dictionnaire
-    #       Afficher l'histogramme et les lettres les plus fréquentes
-    #       Retourner l'histogramme et le tableau de lettres
+    # Afficher l'histogramme et les lettres les plus fréquentes
+    # Retourner l'histogramme et le tableau de lettres
 
-    return {}, []
+    hist = {}
+    for char in sentence:
+        hist[char] += 1
+    else:
+        hist[char] = 1
+
+frequence_seuil = 5
+most_frequent_chars = [key for key, value in histogram.items() if value > frequence_seuil and key != " "]
+
+    return hist, sorted(most_frequent_chars)
 
 
 def get_recipes():
@@ -46,25 +83,25 @@ def print_recipe(ingredients) -> None:
 
 
 def main() -> None:
-    print(f"On essaie d'ordonner les valeurs...")
-    order()
+    #print(f"On essaie d'ordonner les valeurs...")
+    #order()
 
-    print(f"On vérifie les anagrammes...")
-    anagrams()
+    #print(f"On vérifie les anagrammes...")
+    #anagrams()
 
-    my_list = [3, 3, 5, 6, 1, 1]
-    print(f"Ma liste contient-elle des doublons? {contains_doubles(my_list)}")
+    #my_list = [3, 3, 5, 6, 1, 1]
+    #print(f"Ma liste contient-elle des doublons? {contains_doubles(my_list)}")
 
-    grades = {"Bob": [90, 65, 20], "Alice": [85, 75, 83]}
-    name, result = best_grades(grades)
-    print(f"{name} a la meilleure moyenne: {result}")
+    #grades = {"Bob": [90, 65, 20], "Alice": [85, 75, 83]}
+    #name, result = best_grades(grades)
+    #print(f"{name} a la meilleure moyenne: {result}")
     
-    print("On enregistre les recettes...")
-    recipes = get_recipes()
+    #print("On enregistre les recettes...")
+    #recipes = get_recipes()
 
     print("On affiche une recette au choix...")
     print_recipe(recipes)
 
 
-if __name__ == '__main__':
-    main()
+    if __name__ == '__main__':
+        main()
